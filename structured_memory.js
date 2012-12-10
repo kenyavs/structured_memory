9517cd264e9ea6bd937d5f82673fb70361de69b1
Alan O'Donnell <alan.m.odonnell@gmail.com>
2012-12-10 16:00:10 -0500

    Don't think we need this

--- structured_memory.js
+++ structured_memory.js
@@ -12,7 +12,6 @@ $(function() {
     },
     // override backbone sync to force a jsonp call
     sync: function(method, model, options){  
-      options.timeout = 10000;  
       options.dataType = "jsonp";  
       return Backbone.sync(method, model, options); 
     },
